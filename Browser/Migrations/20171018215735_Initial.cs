a047b23837da4087a4b2da7e7e847c9ee9f5ae98
arlyon <arlyon@me.com>
2017-10-18 23:01:05 +0100

    stage migrations

---
+++ Browser/Migrations/20171018215735_Initial.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Browser.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Url",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Addon = table.Column<string>(type: "TEXT", nullable: true),
+                    Host = table.Column<string>(type: "TEXT", nullable: true),
+                    Scheme = table.Column<string>(type: "TEXT", nullable: true),
+                    Unidentified = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Url", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    UrlID = table.Column<Guid>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Url_UrlID",
+                        column: x => x.UrlID,
+                        principalTable: "Url",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    UrlID = table.Column<Guid>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Posts_Url_UrlID",
+                        column: x => x.UrlID,
+                        principalTable: "Url",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_UrlID",
+                table: "Blogs",
+                column: "UrlID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UrlID",
+                table: "Posts",
+                column: "UrlID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Url");
+        }
+    }
+}
